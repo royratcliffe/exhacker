8dbd06e603f10c6a1184aeb0438dd074760a12ff
Roy Ratcliffe <royratcliffe@me.com>
2022-06-07 07:03:07 +0100

    feat: input 11 test

--- test/brain_f__k_test.exs
+++ test/brain_f__k_test.exs
@@ -167,6 +167,52 @@ defmodule BrainF__kTest do
     """
   end
 
+  test "input 11" do
+    assert capture_io(
+      "abcdeghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
+      fn ->
+        {:error, _, _, 100_001} =
+          input11()
+          |> to_charlist()
+          |> BrainF__k.f__k()
+      end
+    ) == "nopqrtuvwxyzabcdefghijklmN"
+  end
+
+  def input11 do
+    """
+    ROT13 as mentioned in wiki brainfuck page (a little modified)
+    -,[+                         Read first character and start outer character reading loop
+        -[                       Skip forward if character is 0
+            >>++++[>++++++++<-]  Set up divisor (32) for division loop
+                                  (MEMORY LAYOUT: dividend copy remainder divisor quotient zero zero)
+            <+<-[                Set up dividend (x minus 1) and enter division loop
+                >+>+>-[>>>]      Increase copy and remainder / reduce divisor / Normal case: skip forward
+                <[[>+<-]>>+>]    Special case: move remainder back to divisor and increase quotient
+                <<<<<-           Decrement dividend
+            ]                    End division loop
+        ]>>>[-]+                 End skip loop; zero former divisor and reuse space for a flag
+        >--[-[<->+++[-]]]<[         Zero that flag unless quotient was 2 or 3; zero quotient; check flag
+            ++++++++++++<[       If flag then set up divisor (13) for second division loop
+                                  (MEMORY LAYOUT: zero copy dividend divisor remainder quotient zero zero)
+                >-[>+>>]         Reduce divisor; Normal case: increase remainder
+                >[+[<+>-]>+>>]   Special case: increase remainder / move it back to divisor / increase quotient
+                <<<<<-           Decrease dividend
+            ]                    End division loop
+            >>[<+>-]             Add remainder back to divisor to get a useful 13
+            >[                   Skip forward if quotient was 0
+                -[               Decrement quotient and skip forward if quotient was 1
+                    -<<[-]>>     Zero quotient and divisor if quotient was 2
+                ]<<[<<->>-]>>    Zero divisor and subtract 13 from copy if quotient was 1
+            ]<<[<<+>>-]          Zero divisor and add 13 to copy if quotient was 0
+        ]                        End outer skip loop (jump to here if ((character minus 1)/32) was not 2 or 3)
+        <[-]                     Clear remainder from first division if second division was skipped
+        <.[-]                    Output ROT13ed character from copy and clear it
+        <-,                     Read next character
+    ]                            End character reading loop
+    """
+  end
+
   test "input 13" do
     assert capture_io(
              "Conqueror of British Empire (CWE). All hail Idi Amin!!!",
